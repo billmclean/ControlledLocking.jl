a35909e2f2a388ca0794dfb7ec939bf29f391b6e
William McLean <w.mclean@unsw.edu.au>
2024-05-16 08:24:17 +1000

    Example 1

---
+++ src/ControlledLocking.jl
@@ -0,0 +1,5 @@
+module ControlledLocking
+
+include("submodules/Example1.jl")
+
+end # module ControlledLocking
---
+++ src/submodules/Example1.jl
@@ -0,0 +1,58 @@
+module Example1
+
+using SimpleFiniteElements
+using SimpleFiniteElements.Elasticity: ∫∫f_dot_v!, ∫∫λ_div_u_div_v!, 
+                                       ∫∫2μ_εu_εv!, elasticity_soln, error_norms
+import StaticArrays: SA
+
+function exact_u(x, y, λ)
+    common_term = sin(x) * sin(y) / λ
+    u1 = ( cos(2x) - 1 ) * sin(2y) + common_term
+    u2 = ( 1 - cos(2y) ) * sin(2x) + common_term
+    return SA[u1, u2]
+end
+
+function ∇u(x, y, λ)
+    sx, cx = sincos(x)
+    sy, cy = sincos(y)
+    s2x, c2x = sincos(2x)
+    s2y, c2y = sincos(2y)
+    s2x_s2y = s2x * s2y
+    cx_sy = cx * sy
+    sx_cy = sx * cy
+    ∂₁u₁ = -2s2x_s2y        + cx_sy / λ
+    ∂₂u₁ = 2(c2x - 1) * c2y + sx_cy / λ
+    ∂₁u₂ = 2(1 - c2y) * c2x + cx_sy / λ
+    ∂₂u₂ =  2s2x_s2y        + sx_cy / λ
+    return SA[ ∂₁u₁  ∂₁u₂
+               ∂₂u₁  ∂₂u₂ ]
+end
+
+function ϕ(x, y)
+    return 4sin(2y) * (2cos(2x) - 1)
+end
+
+function f(x, y, λ)
+    ψ1 = 2sin(x) * sin(y) - cos(x+y) 
+    ψ3 = cos(x+y)
+    f1 =  ϕ(x, y) - ψ3 + ψ1 / λ
+    f2 = -ϕ(y, x) - ψ3 + ψ1 / λ
+    return SA[f1, f2]
+end
+
+gD = SA[0.0, 0.0]
+const essential_bcs = [("Top", gD), ("Bottom", gD), ("Left", gD), ("Right", gD)]
+const μ = 1.0
+
+function numerical_errors(λ::Float64, α::Float64, dof::DegreesOfFreedom;
+                          quadrature_level=2)
+    bilinear_forms = Dict("Omega" => [(∫∫λ_div_u_div_v!, λ^α),
+                                      (∫∫2μ_εu_εv!, μ)])
+    linear_funcs = Dict("Omega" => (∫∫f_dot_v!, f, λ))
+    u1h, u2h = elasticity_soln(dof, bilinear_forms, linear_funcs)
+    L2err, H1err = error_norms(u1h, u2h, exact_u, ∇u, dof, 
+                               quadrature_level, λ)
+    return L2err, H1err
+end
+
+end # module
