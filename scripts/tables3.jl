95b46186d7ca41fab9f0905ea1b4bcc328ee13b1
William McLean <w.mclean@unsw.edu.au>
2025-08-26 15:32:48 +1000

    add Example 3 with variable coefficients

---
+++ scripts/tables3.jl
@@ -0,0 +1,91 @@
+import ControlledLocking: reduced_Λ
+import ControlledLocking.Example3: numerical_errors, essential_bcs,
+				   geometry_filename, diam_Ω, λ, μ, μ_bar
+using SimpleFiniteElements
+using Printf
+
+path = joinpath("..", "spatial_domains", geometry_filename)
+gmodel = GeometryModel(path)
+hmax = 0.4
+nrows = 6
+mesh = FEMesh(gmodel, hmax, order=1, save_msh_file=false,
+              refinements=nrows-1, verbosity=2)
+dof = Vector{DegreesOfFreedom}(undef, nrows)
+h = Vector{Float64}(undef, nrows)
+for row in eachindex(mesh)
+    dof[row] = DegreesOfFreedom(mesh[row], essential_bcs)
+    h[row] = max_elt_diameter(mesh[row])
+end
+
+Λ = [100.0, 1_000.0, 10_000.0, 100_000.0]
+nblocks = length(Λ)
+block_method = [(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (4,1), (4,2)]
+
+L2err = Array{Float64}(undef, nrows, nblocks, 2)
+H1err = similar(L2err)
+Λₕ = Array{Float64}(undef, nrows, nblocks)
+
+ncases = 2 * nblocks
+@printf("Using %d threads to handle %d cases.\n", 
+	Threads.nthreads(), ncases)
+start = time()
+@Threads.threads for k = 1:ncases
+    block, method = block_method[k]
+    for row = 1:nrows
+	if method == 1
+	    L2err[row,block,1], 
+	    H1err[row,block,1] = numerical_errors(Λ[block], Λ[block], dof[row]) 
+	else
+	    Λₕ[row,block] = reduced_Λ(h[row], Λ[block], diam_Ω, μ_bar)
+#		λ[block] / ( 1 + λ[block] * h[row] / diam_Ω )
+#            d = diam_Ω / h[row]
+#	    λₕ[row,block] = λ[block] > d ? d : λ[block]
+#            α = min(1, log(d)/log(λ[block]))
+#	    λₕ[row,block] = λ[block]^α
+	    L2err[row,block,2], 
+	    H1err[row,block,2] = numerical_errors(
+                                  Λ[block], Λₕ[row,block], dof[row]) 
+	end
+    end
+end
+elapsed = time() - start
+@printf("PDE solves took %g seconds\n", elapsed)
+
+function print_table(io::IO, err::Array{Float64}, 
+	             block::Int64, title::String)
+    @printf(io, "\n%70s\n", repeat('-', 70))
+    @printf(io, "\n%s\n\n", title)
+    @printf(io, "%12s      %8s           %10s\n", "", "Standard", "Controlled")
+    @printf(io, "%6s  %5s  %8s  %5s    %8s  %5s  %5s\n\n",
+	    "Nₕ", "h", "error", "rate", "error", "rate", "λₕ")
+    for row = 1:nrows
+        Nₕ = 2 * dof[row].num_free
+	@printf(io, "%6d& %5.3f& ", Nₕ, h[row])
+	if row == 1
+	    @printf(io, "%8.2e& %5s&   %8.2e& %5s& %5.1f ", 
+	            err[row,block,1], "", err[row,block,2], "", Λₕ[row,block])
+	else
+	    rate1 = log2(err[row-1,block,1] / err[row,block,1])
+	    rate2 = log2(err[row-1,block,2] / err[row,block,2])
+            @printf(io, "%8.2e& %5.3f&   %8.2e& %5.3f& %5.1f ", 
+                    err[row,block,1], rate1, 
+		    err[row,block,2], rate2, Λₕ[row,block])
+	end
+	@printf(io, "\\\\\n")
+    end
+end
+
+output_file = "table3.txt"
+@printf("Writing output to %s\n", output_file)
+open(output_file, "w") do io
+    @printf(io, "Output from tables3.jl\n")
+    for block = 1:nblocks
+        title = @sprintf("L2 errors, λ = %g", Λ[block])
+        print_table(io, L2err, block, title)
+    end
+    for block = 1:nblocks
+        title = @sprintf("H1 errors, λ = %g", Λ[block])
+        print_table(io, H1err, block, title)
+    end
+end
+
--- src/ControlledLocking.jl
+++ src/ControlledLocking.jl
@@ -2,6 +2,7 @@ module ControlledLocking
 
 include("submodules/Example1.jl")
 include("submodules/Example2.jl")
+include("submodules/Example3.jl")
 
 function optimal_α(h::Float64, λ::Float64, diam_Ω::Float64)
     α = min(1, log(diam_Ω/h) / log(λ))
@@ -9,6 +10,10 @@ end
 
 function reduced_λ(h::Float64, λ::Float64, diam_Ω::Float64)
     λₕ = λ / ( 1 + λ * h / diam_Ω )
+end
+
+function reduced_Λ(h::Float64, Λ::Float64, diam_Ω::Float64, μ_bar::Float64)
+    Λ_h = Λ * μ_bar / ( μ_bar + Λ * h / diam_Ω )
 end
 
 end # module ControlledLocking
