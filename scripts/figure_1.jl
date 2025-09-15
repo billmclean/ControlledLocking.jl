8d87165db89e50915831eeb23feee58db8a348bb
William McLean <w.mclean@unsw.edu.au>
2025-09-15 14:21:10 +1000

    tweaks to figure 1

--- scripts/figure_1.jl
+++ scripts/figure_1.jl
@@ -7,10 +7,12 @@ using Printf
 
 path = joinpath("..", "spatial_domains", geometry_filename)
 gmodel = GeometryModel(path)
-hmax = 0.03
-mesh = FEMesh(gmodel, hmax)
-dof = DegreesOfFreedom(mesh, essential_bcs)
-h = max_elt_diameter(mesh)
+hmax = 0.4
+nrows = 5
+mesh = FEMesh(gmodel, hmax, order=1, save_msh_file=false,
+              refinements=nrows-1, verbosity=2)
+dof = DegreesOfFreedom(mesh[nrows], essential_bcs)
+h = max_elt_diameter(mesh[nrows])
 
 λ = 100_000.0
 λₕ = range(40, 160, 16)
@@ -18,7 +20,9 @@ L = diam_Ω
 λₕ_star = λ / sqrt(1 + (λ*h/L)^2)
 λₕ_tilde = min(λ, L/h)
 λₕ_sharp = reduced_λ(h, λ, L)
-@printf("Number of unknowns Nₕ = %d\n", dof.num_free)
+Nₕ = 2 * dof.num_free
+@printf("λ = %g\n", λ)
+@printf("Number of unknowns Nₕ = %d\n", Nₕ)
 @printf("Mesh size h = %0.3f\n", h)
 @printf("λₕ star  = %0.2f\nλₕ tilde = %0.2f\nλₕ sharp = %0.2f\n",
         λₕ_star, λₕ_tilde, λₕ_sharp)
@@ -37,7 +41,7 @@ grid(true)
 xlabel(L"$\lambda_h$")
 savefig("error_vs_lambda_h.pdf")
 
-long_λₕ = logrange(10, 1_000, 16)
+long_λₕ = logrange(10, 10_000, 16)
 long_L2err = zeros(length(λₕ))
 long_H1err = zeros(length(λₕ))
 for k in eachindex(λₕ)
